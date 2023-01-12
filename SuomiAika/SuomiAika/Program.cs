b7eeeae6a2b215229dee413e2c98189ddb31f18d
Petri Nurmi <pete874@hotmail.com>
2023-01-12 09:16:26 +0000

    .

--- SuomiAika/SuomiAika/Program.cs
+++ SuomiAika/SuomiAika/Program.cs
@@ -2,9 +2,10 @@
 {
     private static void Main(string[] args)
     {
-        DateTime currentTime = TimeZoneInfo.ConvertTime(DateTime.Now, TimeZoneInfo.FindSystemTimeZoneById("E. Europe Standard Time"));
+        DateTime currentTime = TimeZoneInfo.ConvertTime(DateTime.UtcNow, TimeZoneInfo.FindSystemTimeZoneById("E. Europe Standard Time"));
         var today = DateTime.Now;
         Console.WriteLine("Tietokoneen päivämäärä ja aika " + today);
-        Console.WriteLine("Tietokoneen aika muutettuna suomen ajaksi: " + currentTime);
+        string muutettu = currentTime.ToString("dd.MM.yyyy hh:mm");
+        Console.WriteLine("Tietokoneen aika muutettuna suomen aikavyöhykkeeseen: " + muutettu);
     }
 }
