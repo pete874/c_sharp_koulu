f3e818e9843bd782004903c6f154e473ed7d8b46
Petri Nurmi <pete874@hotmail.com>
2023-01-12 10:52:15 +0200

    .

---
+++ SuomiAika/SuomiAika/Program.cs
@@ -0,0 +1,8 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        var today = DateTime.Now;
+        Console.WriteLine("Tietokoneen päivämäärä ja aika " + today);
+    }
+}
