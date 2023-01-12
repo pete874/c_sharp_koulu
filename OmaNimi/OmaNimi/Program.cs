72f2722b6e1c1bcef5e6c181ae86cfcadd242524
Petri Nurmi <pete874@hotmail.com>
2023-01-12 10:03:14 +0200

    .

--- OmaNimi/OmaNimi/Program.cs
+++ OmaNimi/OmaNimi/Program.cs
@@ -1,3 +1,6 @@
-﻿Console.Write("Anna nimesi: ");
-string nimi = Console.ReadLine();
-Console.WriteLine(nimi);
+﻿Console.Write("Anna etunimesi: ");
+string enimi = Console.ReadLine();
+Console.Write("Anna sukunimesi: ");
+string snimi = Console.ReadLine();
+Console.WriteLine("Tervetuloa " + enimi + " " + snimi);
+Thread.Sleep(5000);
