0631ffadb47e92fa70edfea31a19f951ac03c552
Petri Nurmi <pete874@hotmail.com>
2023-01-12 09:09:56 +0200

    .

--- ReadlineHarjoitus/ReadlineHarjoitus/Program.cs
+++ ReadlineHarjoitus/ReadlineHarjoitus/Program.cs
@@ -1,3 +1,4 @@
-﻿Console.WriteLine("Anna nimesi: "); string nimi = Console.ReadLine();
+﻿Console.WriteLine("Anna nimesi: ");
+string nimi = Console.ReadLine();
 Console.WriteLine("Tervetuloa " + nimi);
-Thread.Sleep(5000);
+Thread.Sleep(5000); moi
