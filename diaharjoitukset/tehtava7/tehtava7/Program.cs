404deec2046c4eb0be838cae30e0d99d98ae04a5
Petri Nurmi <pete874@hotmail.com>
2023-01-18 16:25:49 +0200

    .

---
+++ diaharjoitukset/tehtava7/tehtava7/Program.cs
@@ -0,0 +1,50 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Anna numero 0-999 väliltä, niin muutetaan se sanoiksi");
+        int luku = int.Parse(Console.ReadLine());
+        string kymmenet = "";
+
+
+        switch(luku)
+        {
+            case 1:
+                kymmenet += "yksi";
+                Console.WriteLine(kymmenet);
+                break;
+            case 2:
+                kymmenet += "kaksi";
+                Console.WriteLine(kymmenet);
+                break;
+            case 3:
+                kymmenet += "kolme";
+                Console.WriteLine(kymmenet);
+                break;
+            case 4:
+                kymmenet += "neljä";
+                Console.WriteLine(kymmenet);
+                break;
+            case 5:
+                kymmenet += "viisi";
+                Console.WriteLine(kymmenet);
+                break;
+            case 6:
+                kymmenet += "kuusi";
+                Console.WriteLine(kymmenet);
+                break;
+            case 7:
+                kymmenet += "seitsemän";
+                Console.WriteLine(kymmenet);
+                break;
+            case 8:
+                kymmenet += "kahdeksan";
+                Console.WriteLine(kymmenet);
+                break;
+            case 9:
+                kymmenet += "yhdeksän";
+                Console.WriteLine(kymmenet);
+                break;
+        }
+    }
+}
