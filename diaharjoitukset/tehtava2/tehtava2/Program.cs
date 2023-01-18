f6f9e827846b55facee2aaee12efbb05c9fc62fa
Petri Nurmi <pete874@hotmail.com>
2023-01-18 14:15:19 +0200

    .

---
+++ diaharjoitukset/tehtava2/tehtava2/Program.cs
@@ -0,0 +1,33 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        //pyydetään käyttäjältä kaksi lukua ja tallennetaan ne muuttujiin
+
+        Console.WriteLine("Anna ensimmäinen luku: ");
+        int luku1 = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("Anna toinen luku: ");
+        int luku2 = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("Anna kolmas luku: ");
+        int luku3 = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("Annoit luvut: " + luku1 +", " + luku2 + ", " + luku3);
+
+        //Tehdään if-lause, jolla etsitään isoin luku käyttäjän antamista luvuista
+
+        if (luku1 > luku2 && luku1 > luku3)
+        {
+            Console.WriteLine("isoin luku on: " + luku1);
+        }
+        if (luku2 > luku1 && luku2 > luku3)
+        {
+            Console.WriteLine("isoin luku on: " + luku2);
+        }
+        else
+        {
+            Console.WriteLine("isoin luku on: " + luku3);
+        }
+    }
+}
---
+++ diaharjoitukset/tehtava3/tehtava3/Program.cs
@@ -0,0 +1,58 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        //pyydetään käyttäjältä luku ja tallennetaan se muuttujaan
+        Console.WriteLine("Anna luku 0-9 välillä, niin muutetaan se sanaksi: ");
+        int luku1 = int.Parse(Console.ReadLine());
+
+        
+        //tehdään switch case, joka tulostaa lauseen käyttäjän antamana luvun perusteella
+        switch(luku1)
+        {
+            case 0:
+                Console.WriteLine("En syönyt yhtään pannukakkua");
+                break;
+
+            case 1:
+                Console.WriteLine("Söin yhden pannukakkua");
+                break;
+
+            case 2:
+                Console.WriteLine("Söin kaksi pannukakkua");
+                break;
+
+            case 3:
+                Console.WriteLine("Söin kolme pannukakkua");
+                break;
+
+            case 4:
+                Console.WriteLine("Söin neljä pannukakkua");
+                break;
+
+            case 5:
+                Console.WriteLine("Söin viisi pannukakkua");
+                break;
+
+            case 6:
+                Console.WriteLine("Söin kuusi pannukakkua");
+                break;
+
+            case 7:
+                Console.WriteLine("Söin seitsemän pannukakkua");
+                break;
+
+            case 8:
+                Console.WriteLine("Söin kahdeksan pannukakkua");
+                break;
+
+            case 9:
+                Console.WriteLine("Söin yhdeksän pannukakkua");
+                break;
+
+            default: 
+                Console.WriteLine("Luku ei ollut 0-9 väliltä");
+                break;
+        }
+    }
+}
