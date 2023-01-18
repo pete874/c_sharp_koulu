f69fcd0b0c9dd3e78931a96b6fc308f8af336d04
Petri Nurmi <pete874@hotmail.com>
2023-01-18 14:52:49 +0200

    .

---
+++ diaharjoitukset/tehtava5/tehtava5/Program.cs
@@ -0,0 +1,24 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Haluatko antaa kokonaisluvun, double-luvun vai merkkijonon: ");
+        string luku = Console.ReadLine();
+
+        switch(luku)
+        {
+            case "kokonaisluvun":
+                Console.WriteLine("Anna kokonaisluku: ");
+                int kokonaisluku = int.Parse(Console.ReadLine());
+                kokonaisluku++;
+                Console.WriteLine("lisään antamaasi lukuun yhden, lukusi on nyt: " + kokonaisluku);
+                break;
+
+            case "double-luvun":
+                Console.WriteLine("Anna double-luku: ");
+                double doubleLuku = float.Parse(Console.ReadLine());
+                Console.WriteLine(doubleLuku);
+                break;
+        }
+    }
+}
