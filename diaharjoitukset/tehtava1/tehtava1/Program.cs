73bce4cb54808448fdb7ab93bef886f63f7b2664
Petri Nurmi <pete874@hotmail.com>
2023-01-18 13:50:50 +0200

    .

---
+++ diaharjoitukset/tehtava1/tehtava1/Program.cs
@@ -0,0 +1,26 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        
+        
+        //pyydetään käyttäjältä kaksi lukua ja tallennetaan ne muuttujiin
+        
+        Console.WriteLine("Anna luku: ");
+        int luku1 = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("Anna luku: ");
+        int luku2 = int.Parse(Console.ReadLine());
+
+        //Tehdään if-lause, jolla tulostetaan suuruus järjestykseen käyttäjän antamat luvut
+
+        if (luku1 < luku2)
+        {
+            Console.WriteLine(luku1 + ", " + luku2);
+        }
+        else
+        {
+            Console.WriteLine(luku2 + ", " + luku1);
+        }
+    }
+}
