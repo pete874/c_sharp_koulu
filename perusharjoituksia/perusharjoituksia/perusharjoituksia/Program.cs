2c1752e79c516ea05f2cb1c938b58f1b9cc49fd1
Petri Nurmi <pete874@hotmail.com>
2023-01-18 13:34:55 +0200

    .

---
+++ perusharjoituksia/perusharjoituksia/perusharjoituksia/Program.cs
@@ -0,0 +1,14 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        // Hei maailma tehtävä
+        Console.WriteLine("Hei Maailma!");
+
+        //plus lasku
+        int luku1 = 2;
+        int luku2 = 3;
+        int summa = luku1 + luku2;
+        Console.WriteLine(summa);
+    }
+}
