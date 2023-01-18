15e44dd87e4c616d2a86bd9da56d96c5412a5008
Petri Nurmi <pete874@hotmail.com>
2023-01-18 12:43:02 +0200

    poisto

--- perusharjoituksia/miinuslasku/miinuslasku/Program.cs
+++
@@ -1,8 +0,0 @@
-﻿internal class Program
-{
-    private static void Main(string[] args)
-    {
-        int miinus = 3 - 1;
-        Console.WriteLine(miinus);
-    }
-}
--- perusharjoituksia/pluslasku/pluslasku/Program.cs
+++
@@ -1,8 +0,0 @@
-﻿internal class Program
-{
-    private static void Main(string[] args)
-    {
-        int plus = 1 + 2; 
-        Console.WriteLine(plus);
-    }
-}
