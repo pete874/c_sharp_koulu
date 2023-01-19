ca0bbfb31a2e48b57bedfae75f5f6d72e25fe32c
Petri Nurmi <pete874@hotmail.com>
2023-01-19 11:51:26 +0200

    .

--- diaharjoitukset2/tehtava1/tehtava1/Program.cs
+++ diaharjoitukset2/tehtava1/tehtava1/Program.cs
@@ -6,6 +6,6 @@
         int luku1 = int.Parse(Console.ReadLine());
         int luku2 = int.Parse(Console.ReadLine());
         int summa = luku1 + luku2;
-        Console.WriteLine(summa);
+        Console.WriteLine("Tulos on: " + summa);
     }
 }
