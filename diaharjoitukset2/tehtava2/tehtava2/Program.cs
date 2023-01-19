e46da9e3807c28256a5c51ed2cd827ad663f597e
Petri Nurmi <pete874@hotmail.com>
2023-01-19 10:50:56 +0200

    .

---
+++ diaharjoitukset2/tehtava2/tehtava2/Program.cs
@@ -0,0 +1,5 @@
+﻿Console.WriteLine("Muuttenaan celsius-asteet fahrenheit-asteiksi: ");
+double celsius = Double.Parse(Console.ReadLine());
+double fahrenheit = (double)((celsius * 1.8) + 32);
+
+Console.WriteLine(fahrenheit);
