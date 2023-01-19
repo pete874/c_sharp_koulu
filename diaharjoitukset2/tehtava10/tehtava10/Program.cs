4472722c8a3738769ace690305c7c32d2eb72ed8
Petri Nurmi <pete874@hotmail.com>
2023-01-19 12:09:14 +0200

    .

---
+++ diaharjoitukset2/tehtava10/tehtava10/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+alku:
+Console.WriteLine("Anna kokonaisluku 1-10 väliltä: ");
+int luku = Int32.Parse(Console.ReadLine());
+Console.WriteLine("Kertotaulu antamallesi numerolle: ");
+
+for (int i = 1; i < 11; i++)
+{
+    int lasku = i * luku;
+    Console.WriteLine(luku + " * " + i + " = " + lasku);
+}
+goto alku;
