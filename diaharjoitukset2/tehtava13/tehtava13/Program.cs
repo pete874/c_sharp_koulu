70b23fab59f65398e95c444923968af18625535d
Petri Nurmi <pete874@hotmail.com>
2023-01-19 13:00:03 +0200

    .

---
+++ diaharjoitukset2/tehtava13/tehtava13/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+alku:
+Console.WriteLine("Anna nimesi: ");
+string nimi = Console.ReadLine();
+
+char[] nimiArray = nimi.ToCharArray();
+Array.Reverse(nimiArray);
+string nimiString = string.Join("", nimiArray);
+Console.WriteLine(nimiString);
+goto alku;
