385eee2ef751ea7a5d5d3e9f468f1efb9b9168f9
Petri Nurmi <pete874@hotmail.com>
2023-01-19 10:32:16 +0200

    .

---
+++ diaharjoitukset2/tehtava1/tehtava1/Program.cs
@@ -0,0 +1,10 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int luku1 = 1;
+        int luku2 = 2;
+        int summa = luku1 + luku2;
+        Console.WriteLine(summa);
+    }
+}
