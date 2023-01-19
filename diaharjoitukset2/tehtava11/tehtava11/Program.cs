b2f855e6d9dedd89da57258a3aaf96fe9a2b65be
Petri Nurmi <pete874@hotmail.com>
2023-01-19 12:13:20 +0200

    .

---
+++ diaharjoitukset2/tehtava11/tehtava11/Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+alku:
+Console.WriteLine("Anna ikäsi: ");
+int ika = Int32.Parse(Console.ReadLine());
+Console.WriteLine(ika + " - näytät ikäistäsi nuoremmalta");
+goto alku;
