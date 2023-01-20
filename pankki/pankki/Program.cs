87d6a4a62f91c60923c2c8f7f66a4ea31e448963
Petri Nurmi <pete874@hotmail.com>
2023-01-20 10:36:26 +0200

    .

---
+++ pankki/pankki/Program.cs
@@ -0,0 +1,62 @@
+﻿using Pankki;
+
+namespace Pankki
+{
+    public class Pankkitili
+    {
+        private readonly string m_asiakkaanNimi;
+        private double m_saldo;
+
+        private Pankkitili() { }
+
+        public Pankkitili(string asiakkaanNimi, double saldo)
+        {
+            m_asiakkaanNimi = asiakkaanNimi;
+            m_saldo = saldo;
+        }
+
+        public string AsiakkaanNimi
+        {
+            get { return m_asiakkaanNimi; }
+        }
+
+        public double Saldo
+        {
+            get { return m_saldo; }
+        }
+
+        public void Otto(double summa)
+        {
+            if (summa > m_saldo)
+            {
+                throw new ArgumentOutOfRangeException("summa");
+            }
+            if (summa < 0)
+            {
+                throw new ArgumentOutOfRangeException("summa");
+            }
+            m_saldo += summa;
+        }
+
+        public void Pano(double summa)
+        {
+            if (summa < 0)
+            {
+                throw new ArgumentOutOfRangeException("summa");
+            }
+            m_saldo += summa;
+        }
+
+        public static void Main ()
+        {
+            Pankkitili pt = new Pankkitili("Petri Nurmi", 1200);
+            pt.Otto(300);
+            pt.Pano(400);
+            Console.WriteLine("Saldosi on {0}", pt.Saldo);
+        }
+
+    }
+
+
+}
+
