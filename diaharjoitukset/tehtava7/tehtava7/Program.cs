85813b5b51fd13f0762d0a0d852587fb6b5b8e5c
Petri Nurmi <pete874@hotmail.com>
2023-01-20 10:52:57 +0200

    .

--- diaharjoitukset/tehtava7/tehtava7/Program.cs
+++ diaharjoitukset/tehtava7/tehtava7/Program.cs
@@ -2,49 +2,21 @@
 {
     private static void Main(string[] args)
     {
-        Console.WriteLine("Anna numero 0-999 väliltä, niin muutetaan se sanoiksi");
-        int luku = int.Parse(Console.ReadLine());
-        string kymmenet = "";
+    alku:
+        int numero;
+        Console.WriteLine("Kirjoita numero 0-999 väliltä, niin muutan sen sanaksi: ");
 
-
-        switch(luku)
+        if (numero > 999 | numero <0)
         {
-            case 1:
-                kymmenet += "yksi";
-                Console.WriteLine(kymmenet);
-                break;
-            case 2:
-                kymmenet += "kaksi";
-                Console.WriteLine(kymmenet);
-                break;
-            case 3:
-                kymmenet += "kolme";
-                Console.WriteLine(kymmenet);
-                break;
-            case 4:
-                kymmenet += "neljä";
-                Console.WriteLine(kymmenet);
-                break;
-            case 5:
-                kymmenet += "viisi";
-                Console.WriteLine(kymmenet);
-                break;
-            case 6:
-                kymmenet += "kuusi";
-                Console.WriteLine(kymmenet);
-                break;
-            case 7:
-                kymmenet += "seitsemän";
-                Console.WriteLine(kymmenet);
-                break;
-            case 8:
-                kymmenet += "kahdeksan";
-                Console.WriteLine(kymmenet);
-                break;
-            case 9:
-                kymmenet += "yhdeksän";
-                Console.WriteLine(kymmenet);
-                break;
+            Console.WriteLine("Väärä numero. Anna numero 0-999 väliltä")
+            goto alku;
         }
+
+        else if (numero < 10)
+        {
+            string ykkoset = ykkoset(numero);
+            Console.WriteLine(ykkoset);
+        }
+        
     }
 }
