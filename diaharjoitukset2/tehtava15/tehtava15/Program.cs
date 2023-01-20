ad14fead7c604e8f1d94b33a64cc9507e91709e7
Petri Nurmi <pete874@hotmail.com>
2023-01-20 09:27:38 +0200

    .

--- diaharjoitukset2/tehtava15/tehtava15/Program.cs
+++ diaharjoitukset2/tehtava15/tehtava15/Program.cs
@@ -1,8 +1,18 @@
 ﻿// See https://aka.ms/new-console-template for more information
+alku:
 Console.WriteLine("Anna lause niin etsin lauseen pisimmän sanan: ");
-string lause = Console.ReadLine();
-int pituus = 0;
-string pisinSana;
-lause.Split(",");
+string lause;
+lause = Console.ReadLine();
+string[] sanat = lause.Split(' ');
+string pisinSana = sanat[0];
 
-Console.WriteLine(lause);
+for (int i = 0; i < sanat.Length; i++)
+{
+    if (sanat[i].Length > pisinSana.Length)
+    {
+        pisinSana = sanat[i];
+    }
+}
+
+Console.WriteLine(pisinSana);
+goto alku;
