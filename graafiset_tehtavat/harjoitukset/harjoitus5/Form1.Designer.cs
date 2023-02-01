6d35104fe04aaa046339bc1e25e3f700597ca688
Petri Nurmi <pete874@hotmail.com>
2023-02-01 13:02:17 +0200

    .

---
+++ graafiset_tehtavat/harjoitukset/harjoitus5/Form1.Designer.cs
@@ -0,0 +1,84 @@
+﻿namespace harjoitus5
+{
+    partial class LukujenJarjestys
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.uusiLukuTB = new System.Windows.Forms.TextBox();
+            this.vastausLB = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(34, 27);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(140, 15);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Anna luku (-999 lopetus):";
+            // 
+            // uusiLukuTB
+            // 
+            this.uusiLukuTB.Location = new System.Drawing.Point(239, 24);
+            this.uusiLukuTB.Name = "uusiLukuTB";
+            this.uusiLukuTB.Size = new System.Drawing.Size(100, 23);
+            this.uusiLukuTB.TabIndex = 1;
+            this.uusiLukuTB.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.uusiLukuTB_KeyPress);
+            // 
+            // vastausLB
+            // 
+            this.vastausLB.AutoSize = true;
+            this.vastausLB.Location = new System.Drawing.Point(34, 97);
+            this.vastausLB.Name = "vastausLB";
+            this.vastausLB.Size = new System.Drawing.Size(38, 15);
+            this.vastausLB.TabIndex = 2;
+            this.vastausLB.Text = "label2";
+            this.vastausLB.Visible = false;
+            // 
+            // LukujenJarjestys
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.vastausLB);
+            this.Controls.Add(this.uusiLukuTB);
+            this.Controls.Add(this.label1);
+            this.Name = "LukujenJarjestys";
+            this.Text = "Luvut järjestykseen";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Label label1;
+        private TextBox uusiLukuTB;
+        private Label vastausLB;
+    }
+}
